b5d9e35eb739726ad4250d87d0e378a6b582a053
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-11-02 02:19:27 +0800

    master.blade.php 的 Bootstrap core JavaScript asset修正

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,7 +19,7 @@
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
   <!-- Custom styles for this template -->
-  <link href="css/clean-blog.min.css" rel="stylesheet">
+  <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -34,14 +34,12 @@
   @include('layouts.partials.footer')
 
   <!-- Bootstrap core JavaScript -->
-  <script src="{{asset('js/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('js/jquery.min.js')}}"></script>
   <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- Contact Form JavaScript -->
-  <!--
   <script src="js/jqBootstrapValidation.js"></script>
   <script src="js/contact_me.js"></script>
-  -->
 
   <!-- Custom scripts for this template -->
   <script src="{{asset('js/clean-blog.min.js')}}"></script>
