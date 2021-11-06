be670fef67c1024fe056eb78b77ade1f9c75efd5
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-11-07 06:05:16 +0800

    0825_u1_練習5-5: 刪除多筆資料(destroy方法)

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,9 @@ Route::get('/', function () {
     //$post = Post::find(1);
     //$post->delete();
 
-    Post::destroy(2);
+    //Post::destroy(2);
+
+    Post::destroy(3, 5, 7);
 });
 
 Route::get('posts', [PostsController::class, 'index'])->name('posts.index');
